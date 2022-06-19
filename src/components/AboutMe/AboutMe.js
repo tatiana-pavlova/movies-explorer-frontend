759f9623ffbf2836e3daf63eb9db8be289e95738
Tatiana Pavlova <pavlova.tat.ole@gmail.com>
2022-06-19 15:32:20 +0300

    refactor: edit author info

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,7 +14,7 @@ function AboutMe() {
         <img src={studentPhoto} className='about-me__photo' alt='Фото студента' />
         <div className='about-me__full-info'>
           <h3 className='about-me__name'>Татьяна</h3>
-          <p className='about-me__position'>Веб-разработчик, 35 лет</p>
+          <p className='about-me__position'>Веб-разработчик, 36 лет</p>
           <p className='about-me__info'>Я живу в Санкт-Петербурге. Являюсь выпускницей МГУ факультета иностранных языков 
             и регионоведения. Люблю английский и французский языки, чтение интересных книг, встречи и общение с друзьями. 
             Сторонник активного образа жизни, занимаюсь плаванием. Однажды благодаря Яндекс Практикум открыла для себя 
